2d0fe6b5e287a7cfe7d4ac903ab9afb083e81c5c
Ivan-Korolenko <iam@ivankorolenko.com>
2020-01-28 06:24:08 +0300

    + HTML minification in production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const base = require('./webpack.base.js')
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = merge(base, {
     plugins: [
@@ -17,6 +18,40 @@ module.exports = merge(base, {
                 }
             },
             canPrint: true
+        }),
+        new HtmlWebpackPlugin({
+            inject: 'head',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeAttributeQuotes: true,
+            },
+            filename: 'en/index.html',
+            template: 'en/index.html',
+            chunks: ['redirect', 'en/index']
+        }),
+        new HtmlWebpackPlugin({
+            inject: 'head',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeAttributeQuotes: true,
+            },
+            filename: 'ru/index.html',
+            template: 'ru/index.html',
+            chunks: ['redirect', 'index']
         })
       ],
     optimization: {
